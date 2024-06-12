def3d100ac3bf22a0471df0a2078c73053b235e4
bao bao <baobao22.work@gmail.com>
2024-06-12 08:48:40 +0700

    lesson 4

---
+++ runCoder.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+
+
+// Main function
+int main (){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int n; cin >> n;
+    int a[n];
+    // INPUT
+    for(int i = 0; i < 100; i++){
+        cin >> a[i];
+    }
+    // ALGORITHM
+
+    // OUTPUT
+    cout << result << endl;
+    cout << n - result << endl;
+
+
+    return 0;
+}
