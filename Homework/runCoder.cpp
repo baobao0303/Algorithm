47025ac936129afe0649c921286771f120b40681
baobao0303 <baobao22.work@gmail.com>
2024-07-24 20:16:45 +0700

    config

--- Homework/runCoder.cpp
+++ Homework/runCoder.cpp
@@ -1,26 +1,15 @@
-#include<bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-using ll = long long;
-
-
-
-// Main function
-int main (){
+int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-    int n; cin >> n;
-    int a[n];
-    // INPUT
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    // ALGORITHM
 
-    // OUTPUT
-    cout << result << endl;
 
     return 0;
 }
